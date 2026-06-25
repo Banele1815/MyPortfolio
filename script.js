267ddd8de5ee075d3f77a7724abb536b7f0a52f4
Gcina Banele Kubeka <137442411+Banele1815@users.noreply.github.com>
2026-06-25 13:09:22 +0200

    made my form to actually work using email.js

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function sendPrompt(text) {
     alert('Case study prompt:\n\n' + text);
   }
 }
+//Making my contact form work with emailjs
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  emailjs.sendForm(
+    "service_hcpaq39",
+    "template_71pimun",
+    this
+  ).then(
+    function () {
+      alert("Message sent successfully 🚀");
+      document.getElementById("contact-form").reset();
+    },
+    function (error) {
+      alert("Failed to send message ❌");
+      console.log(error);
+    }
+  );
+});
