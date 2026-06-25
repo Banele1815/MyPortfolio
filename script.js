296ffdfeb99f8abe85833892ae10c36173899040
Gcina Banele Kubeka <137442411+Banele1815@users.noreply.github.com>
2026-06-25 13:18:32 +0200

    removed the send prompt function

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ function scrollToSection(id) {
   if (el) el.scrollIntoView({ behavior: 'smooth' });
 }
 
-function sendPrompt(text) {
-  if (typeof window.sendPrompt === 'function') {
-    window.sendPrompt(text);
-  } else {
-    alert('Case study prompt:\n\n' + text);
-  }
-}
 //Making my contact form work with emailjs
 document.getElementById("contact-form").addEventListener("submit", function (e) {
   e.preventDefault();
